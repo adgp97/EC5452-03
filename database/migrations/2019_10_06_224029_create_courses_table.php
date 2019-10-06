731a9db2c45d0109f8f92e1d3908324aef044f23
Adrian Gonzalez <adgp391997@gmail.com>
2019-10-06 19:10:20 -0400

    video2/27:07. Adding new fields to courses table

--- database/migrations/2019_10_06_224029_create_courses_table.php
+++ database/migrations/2019_10_06_224029_create_courses_table.php
@@ -15,6 +15,18 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->string('title');
+            $table->longText('description');
+            $table->longText('about_instructor');
+            $table->double('discount_price',10,2);
+            $table->double('actual_price',10,2);
+            $table->string('playlist_url');
+            $table->integer('view_count');
+            $table->integer('subscriber_count');
+            $table->integer('status')->default(0); //live = 1, not live = 0
+            $table->string('photo')->nullable();
+            $table->softDeletes();           
             $table->timestamps();
         });
     }
