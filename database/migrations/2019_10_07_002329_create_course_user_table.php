3b50f99a59250e56c8cf30043eea9485ad096ab9
Adrian Gonzalez <adgp391997@gmail.com>
2019-10-06 20:38:49 -0400

    video2/end. Adding fields to course user table

--- database/migrations/2019_10_07_002329_create_course_user_table.php
+++ database/migrations/2019_10_07_002329_create_course_user_table.php
@@ -15,6 +15,15 @@ class CreateCourseUserTable extends Migration
     {
         Schema::create('course_user', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('user_id');
+            $table->integer('course_id');
+            $table->integer('user_account_id');
+            $table->dateTime('paid_date');
+            $table->dateTime('expity_date');
+            $table->string('plan')->nullable();
+            $table->double('paid_amount')->nullable();
+            $table->tinyInteger('status')->default(0);
+            $table->softDeletes();
             $table->timestamps();
         });
     }
